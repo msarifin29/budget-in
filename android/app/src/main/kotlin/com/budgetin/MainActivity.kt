e0568e652724d8c87150e9f1e1ac44c1a6a9565d
samsul <ms.arifin29@gmail.com>
2024-02-18 21:00:55 +0800

    initial

---
+++ android/app/src/main/kotlin/com/budgetin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.budgetin.budget_in
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:budget_in/app/app.dart';
+import 'package:budget_in/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
