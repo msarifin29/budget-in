34d24b395db59bb01929baa6140dfe02ff1bcc5d
samsul <ms.arifin29@gmail.com>
2024-06-27 11:50:06 +0800

    fix : loading card dashboard

--- lib/features/onboarding/presentation/ui/widgets/chart_dahboard_widget.dart
+++ lib/features/onboarding/presentation/ui/widgets/chart_dahboard_widget.dart
@@ -13,46 +13,49 @@ class ChartDashboardWidget extends StatelessWidget {
     final size = MediaQuery.sizeOf(context);
     return Card(
       margin: const EdgeInsets.symmetric(horizontal: 12),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.all(8),
-            child: BlocBuilder<MonthlyReportDashboardBloc,
-                MonthlyReportDashboardState>(
-              builder: (context, state) {
-                if (state is MonthlyReportDashboardLoading) {
-                  return const CircularLoading();
-                } else if (state is MonthlyReportDashboardFailure) {
-                  return RefreshButton(onPressed: () {
-                    callBack();
-                  });
-                } else if (state is MonthlyReportDashboardSuccess) {
-                  final expenses = state.data.expenses;
-                  return ChartExpenseWidget(expenses: expenses);
-                }
-                return const SizedBox();
-              },
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: size.width * 0.2,
-              vertical: 24,
-            ),
-            child: PrimaryOutlineButton(
-              text: context.l10n.more,
-              onPressed: () {
-                Navigator.pushNamed(
-                  context,
-                  MonthlyReportPage.routeName,
-                  arguments: {},
-                );
-              },
-            ),
-          ),
-        ],
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(8),
+        child: BlocBuilder<MonthlyReportDashboardBloc,
+            MonthlyReportDashboardState>(
+          builder: (context, state) {
+            if (state is MonthlyReportDashboardLoading) {
+              return SizedBox(
+                height: size.height * 0.3,
+                child: const CircularLoading(),
+              );
+            } else if (state is MonthlyReportDashboardFailure) {
+              return RefreshButton(onPressed: () {
+                callBack();
+              });
+            } else if (state is MonthlyReportDashboardSuccess) {
+              final expenses = state.data.expenses;
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ChartExpenseWidget(expenses: expenses),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: size.width * 0.2,
+                      vertical: 24,
+                    ),
+                    child: PrimaryOutlineButton(
+                      text: context.l10n.more,
+                      onPressed: () {
+                        Navigator.pushNamed(
+                          context,
+                          MonthlyReportPage.routeName,
+                          arguments: {},
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              );
+            }
+            return const SizedBox();
+          },
+        ),
       ),
     );
   }
